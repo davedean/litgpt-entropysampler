70def8a706fbbd1af42dc58e2224f6ad5505a115
David Dean <dave.dean@gmail.com>
2024-12-06 20:12:38 +1100

    add test script

---
+++ litgpt/test_reaasoning.sh
@@ -0,0 +1,2 @@
+litgpt generate meta-llama/Llama-3.2-1B-Instruct --max_new_tokens=8000 --prompt 'which is larger? 9.11 or 9.9? think carefully, dont just answer. Think through your response carefully, line by line and explain it all before giving your final answer.'
+
